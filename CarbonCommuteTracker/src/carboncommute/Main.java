7697885718d50ef4e337a72cd13620ac8009f22c
NOOR UL EMAN <nooruleman645@gmail.com>
2026-06-04 13:08:22 +0500

    updated

--- CarbonCommuteTracker/src/carboncommute/Main.java
+++ CarbonCommuteTracker/src/carboncommute/Main.java
@@ -2,16 +2,6 @@ package carboncommute;
 import carboncommute.system.CarbonCommuteSystem;
 /**
  * Main - Application Entry Point
- *
- * CarbonCommute Tracker & Emission Reporter
- * OOP Lab Project | BSAI Section: Orange
- *
- * Authors : Noor Ul Eman (B25F0322AI193)
- *           Hafsa Tahir  (B25F1962AI181)
- * Submitted To: Sir Obaidullah Miakhil
- *
- * Run: javac -d out src/carboncommute/**‌/*.java
- *      java  -cp out carboncommute.Main
  */
 public class Main {
     public static void main(String[] args) {
